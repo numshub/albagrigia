32ce96a20b87a10d338d26475d31581ae2ade2ab
root <root@Thor.localdomain>
2018-10-20 11:53:49 +0200

    Auto-generated commit

--- assets/typings.d.ts
+++ assets/typings.d.ts
@@ -5,7 +5,12 @@ interface Color {
     name: string
 }
 
-declare module "*.json" {
+declare module "colors.json" {
     const value: { [id: string]: Color };
+    export default value;
+}
+
+declare module "default_colors.json" {
+    const value: { [id: string]: string };
     export default value;
 }
